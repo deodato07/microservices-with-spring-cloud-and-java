21f077d0897ea9d3b726b37be930f44f2b6b03bf
deodato07 <deodatomoreira07@gmail.com>
2025-08-25 13:27:20 -0300

    -Manipulando endpoints e criando métodos com tratamentos de erros

--- src/main/java/br/com/deodato07/controllers/MathController.java
+++ src/main/java/br/com/deodato07/controllers/MathController.java
@@ -1,5 +1,6 @@
 package br.com.deodato07.controllers;
 
+import br.com.deodato07.exception.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,12 @@ public class MathController {
             @PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please set a numeric value!");
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
     private Double convertToDouble(String strNumber) throws IllegalArgumentException{
-        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+        if (strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value!");
         String number = strNumber.replaceAll(",", ".");
         return Double.parseDouble(number);
     }
