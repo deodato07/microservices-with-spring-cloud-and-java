de35d1503b1bec03e0d6e920fcf2935f065bc471
deodato07 <deodatomoreira07@gmail.com>
2025-11-17 09:41:35 -0300

    -Mokanco objeto para utilizar get()

--- src/main/java/br/com/deodato07/services/PersonServices.java
+++ src/main/java/br/com/deodato07/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.deodato07.services;
 import br.com.deodato07.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,14 +15,37 @@ public class PersonServices {
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+
+        }
+
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one person!");
+
         Person person = new Person();
+
         person.setId(counter.incrementAndGet());
-        person.setFirstName("Deodato");
+        person.setFirstName("Deodato 1");
         person.setLastName("Moreira");
         person.setAddress("São Jose dos Campos - SP - Brasil");
+        person.setGender("Male");
+        return person;
+    }
 
+    private Person mockPerson(int i) {
+        Person person = new Person();
+
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name" + i);
+        person.setLastName("Last Name" + i);
+        person.setAddress("Some Address in Brasil");
         person.setGender("Male");
         return person;
     }
